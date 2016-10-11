030c92b36e090881eddd559bd4c95431a1fbc97b
Kartik Luke Singh <kartikluke@gmail.com>
2016-10-11 17:18:52 +0530

    fixed another silly mistake. state.selected.date, action.date both should be alternates

--- src/reducers.js
+++ src/reducers.js
@@ -9,31 +9,31 @@ var initialState = {
 };
 
 export default function mainReducer(state = initialState, action) {
-  switch(action.type) {
-    case 'CHANGE_ADDRESS': 
+  switch (action.type) {
+    case 'CHANGE_ADDRESS':
       return Object.assign({}, state, {
         address: action.address
       });
-    case 'SET_TEMPS': 
+    case 'SET_TEMPS':
       return Object.assign({}, state, {
         temps: action.temps
       });
-    case 'SET_DATES': 
+    case 'SET_DATES':
       return Object.assign({}, state, {
         dates: action.dates
       });
-    case 'SET_SELECTED_TEMP': 
+    case 'SET_SELECTED_TEMP':
       return Object.assign({}, state, {
         selected: {
-          temp: action.temp, 
-          date: state.selected.temp
+          date: state.selected.date,
+          temp: action.temp
         }
       });
-    case 'SET_SELECTED_DATE': 
+    case 'SET_SELECTED_DATE':
       return Object.assign({}, state, {
         selected: {
-          date: action.date, 
-          temp: state.selected.date
+          date: action.date,
+          temp: state.selected.temp
         }
       });
     default:
