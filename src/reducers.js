ff9eca4477de56565886c9ac8fb6d27522cb2fcb
Kartik Luke Singh <kartikluke@gmail.com>
2016-10-11 17:12:02 +0530

    fixed reducers

--- src/reducers.js
+++ src/reducers.js
@@ -11,15 +11,31 @@ var initialState = {
 export default function mainReducer(state = initialState, action) {
   switch(action.type) {
     case 'CHANGE_ADDRESS': 
-      return Object.assign({}, state, {address: action.address});
+      return Object.assign({}, state, {
+        address: action.address
+      });
     case 'SET_TEMPS': 
-      return Object.assign({}, state, {temps: action.temps});
+      return Object.assign({}, state, {
+        temps: action.temps
+      });
     case 'SET_DATES': 
-      return Object.assign({}, state, {dates: action.dates});
+      return Object.assign({}, state, {
+        dates: action.dates
+      });
     case 'SET_SELECTED_TEMP': 
-      return Object.assign({}, state, {selected: {temp: action.temp, date: state.selected.temp}});
+      return Object.assign({}, state, {
+        selected: {
+          temp: action.temp, 
+          date: state.selected.temp
+        }
+      });
     case 'SET_SELECTED_DATE': 
-      return Object.assign({}, state, {selected: {date: action.date, temp: state.selected.date}});
+      return Object.assign({}, state, {
+        selected: {
+          date: action.date, 
+          temp: state.selected.date
+        }
+      });
     default:
       return state;
   }
